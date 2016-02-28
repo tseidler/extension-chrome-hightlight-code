b747897d3ecb1043a287d9474780f383beb15d78
Terry Seidler <terry.seidler@ogd.nl>
2016-02-28 13:44:53 +0100

    Make the extension use settings for highlighting, refactor code

--- main.js
+++ main.js
@@ -1,33 +1,70 @@
-// Only add the context menu when the extension is installed/updated
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener(installListener);
+chrome.contextMenus.onClicked.addListener(contextMenuListener);
+chrome.runtime.onMessage.addListener(messageListener);
+
+function installListener() {
   rebuildContextMenusFromOptions();
-});
-
-var default_options = {
-  'lexer':      'javascript',
-  'style':      'colorful',
-  'linenos':    true,
-  'divstyles':  'none'
-};
-var api_uri = 'http://hilite.me/api';
-
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  if(info.menuItemId === 'tseidler_hilite_code') {
-    highlightSelection(info, tab);
+}
+function contextMenuListener(info) {
+  if(info.parentMenuItemId === 'tseidler_hilite_code') {
+    var language = info.menuItemId.slice('tseidler_hilite_language_'.length);
+    highlightSelection(info.selectionText, language);
   }
-});
+}
+function messageListener(request) {
+  if(request === "rebuildContextMenus") {
+    rebuildContextMenusFromOptions();
+  }
+}
 
-chrome.runtime.onMessage.addListener(
-  function(request) {
-    console.log('REQUESTETET', request);
-    if(request === "rebuildContextMenus") {
-      rebuildContextMenusFromOptions();
+function highlightSelection(selection, lexer) {
+  // get settings from storage & do request
+  chrome.storage.sync.get({
+    'highlight_output_style': 'colorful',
+    'highlight_line_number':  true,
+    'highlight_div_styles': 'none'
+  }, function (items) {
+    var settings = {
+      'lexer':      lexer,
+      'style':      items.highlight_output_style,
+      'linenos':    items.highlight_line_number,
+      'divstyles':  items.highlight_div_styles
+    };
+
+    doHilightAPIRequest(selection, settings, copyToClipBoard);
+  });
+}
+
+function doHilightAPIRequest(text, options, callback) {
+  var api_uri = 'http://hilite.me/api';
+  var data = new FormData();
+  data.append("code", text);
+  Object.keys(options).forEach(function (key) {
+    data.append(key, options[key]);
+  });
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+      callback(this.responseText);
     }
-  }
-);
+  });
+  xhr.open("POST", api_uri);
+  xhr.send(data);
+}
 
-function highlightSelection(info) {
-  requestHiliteHTML(info.selectionText, default_options, copyToClipBoard);
+function copyToClipBoard(info) {
+  // copy to clipboard
+  var copyFrom = document.createElement('textarea');
+  var body = document.querySelector('body');
+
+  copyFrom.textContent = info;
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  body.removeChild(copyFrom);
 }
 
 function rebuildContextMenusFromOptions() {
@@ -52,34 +89,4 @@ function createContextMenuForLanguage(language_key) {
     'parentId':   'tseidler_hilite_code',
     'contexts':   ['selection']
   });
-}
-
-function requestHiliteHTML(text, options, callback) {
-  var data = new FormData();
-  data.append("code", text);
-  data.append("lexer", "javascript");
-
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      console.log(this.responseText);
-      callback(this.responseText);
-    }
-  });
-  xhr.open("POST", api_uri);
-  xhr.send(data);
-}
-
-function copyToClipBoard(info) {
-  // copy to clipboard
-  var copyFrom = document.createElement('textarea');
-  var body = document.querySelector('body');
-
-  copyFrom.textContent = info;
-  body.appendChild(copyFrom);
-  copyFrom.select();
-  document.execCommand('copy');
-  body.removeChild(copyFrom);
 }
