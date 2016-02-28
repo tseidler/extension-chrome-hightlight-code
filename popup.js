6ba009f578965c2217b0318fcfff3f22a72e2e0e
Terry Seidler <terry.seidler@ogd.nl>
2016-02-28 13:44:25 +0100

    Update eslint rules

--- popup.js
+++ popup.js
@@ -3,6 +3,6 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   renderStatus('Performing Google Image search for wheeeeeee');
 });
